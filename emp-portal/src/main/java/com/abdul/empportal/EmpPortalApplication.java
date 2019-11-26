e889bdcfd5fe9f68d5a3c86bc993a90134a9dc70
abdul razak <abdul.razak.shaik@cgi.com>
2019-11-26 12:55:39 +0200

    first commit

---
+++ emp-portal/src/main/java/com/abdul/empportal/EmpPortalApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.empportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPortalApplication.class, args);
+	}
+
+}
---
+++ emp-portal/src/test/java/com/abdul/empportal/EmpPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.empportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
